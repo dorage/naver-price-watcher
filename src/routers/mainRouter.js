fe7df19cb712536d2ff545151c8ee416c294894f
Dorage-San <baloonflower554@gmail.com>
2021-02-03 01:04:35 +0900

    readme 수정

--- src/routers/mainRouter.js
+++ src/routers/mainRouter.js
@@ -1,16 +1,7 @@
 import { Router } from 'express';
-<<<<<<< HEAD
-import { getHome } from '../controllers/mainController';
-=======
-import { getHome, getLatest, postUpdate } from '../controllers/mainController';
->>>>>>> 1528449be65435bfb6ed4b8dd96e81dbca3b2285
 
 const router = new Router();
 
 router.get('/', getHome);
-
-router.get('/get');
-
-router.post('/update', postUpdate);
 
 export default router;
